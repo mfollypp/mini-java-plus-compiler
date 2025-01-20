d64edce92ccea35cfa8c5307799de8010f3419d4
Matheus Folly <matheusfolly@id.uff.br>
2025-01-20 18:08:57 -0300

    merge folly semantic

--- output.asm
+++ output.asm
@@ -2,7 +2,6 @@
 main:
 sw $fp 0($sp)
 sw $a0 0($sp)
-<<<<<<< Updated upstream
 addiu $sp, $sp, -4
 addiu $sp, $sp, -4
 li $a0, 10
@@ -15,26 +14,6 @@ sw $a0 0($sp)
 addiu $sp, $sp, -4
 sw $a0 0($sp)
 addiu $sp, $sp, -4
-=======
-addiu $sp, $sp, -4
-addiu $sp, $sp, -4
-li $a0, 20
-sw $a0 0($sp)
-addiu $sp, $sp, -4
-sw $a0 0($sp)
-addiu $sp, $sp, -4
-li $a0, 10
-sw $a0 0($sp)
-addiu $sp, $sp, -4
-sw $a0 0($sp)
-addiu $sp, $sp, -4
-jal |Fac|ComputeFac
-sw $a0 0($sp)
-addiu $sp, $sp, -4
-lw $t0 4($sp)
-addiu $sp, $sp, 4
-add $a0, $a0, $t0
->>>>>>> Stashed changes
 sw $a0 0($sp)
 addiu $sp, $sp, -4
 lw $a0 4($sp)
@@ -47,19 +26,11 @@ sw $a0 0($sp)
 addiu $sp, $sp, -4
 lw $a0 4($sp)
 addiu $sp, $sp, 4
-<<<<<<< Updated upstream
-=======
-sw $a0 0($sp)
-addiu $sp, $sp, -4
-lw $a0 4($sp)
-addiu $sp, $sp, 4
->>>>>>> Stashed changes
 li $v0, 1
 syscall
 li $v0, 10
 syscall
 Fac:
-<<<<<<< Updated upstream
 jr $ra
 _Fac_ComputeFac_ComputeFac:
 move $fp $sp
@@ -95,7 +66,5 @@ sw $a0 0($sp)
 addiu $sp, $sp, -4
 lw $a0 4($sp)
 addiu $sp, $sp, 4
-=======
->>>>>>> Stashed changes
 lw $fp 0($sp)
 jr $ra
